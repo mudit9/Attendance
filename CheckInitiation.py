5e5e3a29a3607102f6cb2a21359b52b8b49b7523
mudit9 <msgagent1@gmail.com>
2017-09-01 07:48:48 +0530

    Check

---
+++ CheckInitiation.py
@@ -0,0 +1,20 @@
+import time
+import mechanicalsoup
+
+browser = mechanicalsoup.Browser()
+login_page = browser.get("https://markattendance.webapps.snu.edu.in/public/application/login/login")
+login_form = login_page.soup.select("form")[0]
+login_form.select("#login_user_name")[0]['value'] = "ms418"
+login_form.select("#login_password")[0]['value'] = "Rakhi@17"
+page2 = browser.submit(login_form, login_page.url)	
+while 1:
+    page2 = browser.submit(login_form, login_page.url)
+    time.sleep(10)
+    page3 = browser.submit(login_form, login_page.url)
+    if(page2.text!=page2.text):
+        print("Initiated!")
+        break
+    else:
+        print('Not yet..')
+ 
+
