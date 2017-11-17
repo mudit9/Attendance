5f9af2f4a6f76cc42fedd839c548ff46168311a9
mudit9 <msgagent1@gmail.com>
2017-11-17 06:58:42 +0530

    done

---
+++ x.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.math.*;
+
+public class x
+{
+ public static void main( String[] args )
+     {Random ran = new Random();
+int xa = ran.nextInt(2);
+System.out.println(xa);
+}
+}
+
+
