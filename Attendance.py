95d76e04b36f7aadcaebf41b9a31199aedf3bf77
mudit9 <msgagent1@gmail.com>
2017-11-18 07:17:36 +0530

    d

--- Attendance.py
+++ Attendance.py
@@ -6,7 +6,7 @@ your_id = "enter your id here "
 your_password = "enter your password here" 
 login_page = browser.get("https://markattendance.webapps.snu.edu.in/public/application/login/login")
 login_form = login_page.soup.select("form")[0]
-#mechanicalSoup.
+#mechanicalSoup
 login_form.select("#login_user_name")[0]['value'] = your_id
 login_form.select("#login_password")[0]['value'] = your_password
 page2 = browser.submit(login_form, login_page.url)
